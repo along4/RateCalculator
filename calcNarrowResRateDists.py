2bad15f5abc94d4e973f6f936a0d3eceb6d97ca6
Alexander Long <along4@nd.edu>
2016-04-07 08:48:45 -0400

    created single rate calc program

--- calcNarrowResRateDists.py
+++ calcNarrowResRateDists.py
@@ -22,10 +22,10 @@ import ROOT
 #........................................................................................................
 #	Files to read in and read out to.
 #........................................................................................................
-resInfoFile = '../34Ar+a/ResonanceInfo.dat'
+resInfoFile = '../30S+a/ResonanceInfo.dat'
 
 
-outputRootFile = "../34Ar+a/calcRate/"+ sys.argv[1] + ".root"
+outputRootFile = "../30S+a/calcRate/"+ sys.argv[1] + ".root"
 
 #	Creating a dictionary to read resonance parameters into.
 #........................................................................................................
@@ -93,18 +93,17 @@ def calcRateAtTemp(resonanceEnergies,resonanceStrengths,temperature,reducedMass)
 #	
 #........................................................................................................
 def calcReactionRate(resonances,temps,Thres,masses):
-	alphaStrength =  0.15
+	alphaStrength =  0.01
 	protonStrength = 0.01
 	reducedMass = masses[0]*masses[1]/(masses[0]+masses[1])
 	resonanceEnergies = [calcResonacneEnergy(resonances,exState,Thres) for exState in sorted(resonances, key=resonances.get, reverse=False)]
 	resonanceStrengths = [calcResonanceStrength(random.randrange(0,5),resonances,exState,protonStrength,alphaStrength) for exState in sorted(resonances, key=resonances.get, reverse=False)]
         
-    # Creating one posibble alpha cluster state
-    i = random.randint(0,4)
-    resonanceStrengths[i] = resonanceStrengths[i]*10
+        # Creating one posibble alpha cluster state
+        i = random.randint(0,4)
+        resonanceStrengths[i] = resonanceStrengths[i]*10
 
-    reactionRate = [calcRateAtTemp(resonanceEnergies,resonanceStrengths,temperature,reducedMass) for temperature in temps]
-	
+        reactionRate = [calcRateAtTemp(resonanceEnergies,resonanceStrengths,temperature,reducedMass) for temperature in temps]
 	return reactionRate
 
 
@@ -135,14 +134,104 @@ with open(resInfoFile,'r') as resInfoFile:
 
 		resonances.update({float(data[0]):{'error':float(data[1]),0:res0Parameters,1:res1Parameters,2:res2Parameters,3:res3Parameters,4:res4Parameters}})
 
-#
-#........................................................................................................
-numRate = calcReactionRate(resonances,temps,alphaThres,masses)
-	
-	
-for temp in range(len(temps):
-	print numRate[i]
 
+
+#	
+#........................................................................................................
+outputfile = ROOT.TFile(outputRootFile,"recreate")
+tempTree = ROOT.TTree("temps", "Reaction rate of 30S(a,p) at various temps")
+
+#	
+#........................................................................................................
+t0010 = np.zeros(1, dtype=float)
+t0015 = np.zeros(1, dtype=float)
+t0020 = np.zeros(1, dtype=float)
+t0030 = np.zeros(1, dtype=float)
+t0040 = np.zeros(1, dtype=float)
+t0050 = np.zeros(1, dtype=float)
+t0060 = np.zeros(1, dtype=float)
+t0070 = np.zeros(1, dtype=float)
+t0080 = np.zeros(1, dtype=float)
+t0090 = np.zeros(1, dtype=float)
+t0100 = np.zeros(1, dtype=float)
+t0150 = np.zeros(1, dtype=float)
+t0200 = np.zeros(1, dtype=float)
+t0250 = np.zeros(1, dtype=float)
+t0300 = np.zeros(1, dtype=float)
+t0350 = np.zeros(1, dtype=float)
+t0400 = np.zeros(1, dtype=float)
+t0500 = np.zeros(1, dtype=float)
+t0600 = np.zeros(1, dtype=float)
+t0700 = np.zeros(1, dtype=float)
+t0800 = np.zeros(1, dtype=float)
+t0900 = np.zeros(1, dtype=float)
+t1000 = np.zeros(1, dtype=float)
+
+#	
+#........................................................................................................
+tempTree.Branch('temp_0010', t0010, 't9_0010/D')
+tempTree.Branch('temp_0015', t0015, 't9_0015/D')
+tempTree.Branch('temp_0020', t0020, 't9_0020/D')
+tempTree.Branch('temp_0030', t0030, 't9_0030/D')
+tempTree.Branch('temp_0040', t0040, 't9_0040/D')
+tempTree.Branch('temp_0050', t0050, 't9_0050/D')
+tempTree.Branch('temp_0060', t0060, 't9_0060/D')
+tempTree.Branch('temp_0070', t0070, 't9_0070/D')
+tempTree.Branch('temp_0080', t0080, 't9_0080/D')
+tempTree.Branch('temp_0090', t0090, 't9_0090/D')
+tempTree.Branch('temp_0100', t0100, 't9_0100/D')
+tempTree.Branch('temp_0150', t0150, 't9_0150/D')
+tempTree.Branch('temp_0200', t0200, 't9_0200/D')
+tempTree.Branch('temp_0250', t0250, 't9_0250/D')
+tempTree.Branch('temp_0300', t0300, 't9_0300/D')
+tempTree.Branch('temp_0350', t0350, 't9_0350/D')
+tempTree.Branch('temp_0400', t0400, 't9_0400/D')
+tempTree.Branch('temp_0500', t0500, 't9_0500/D')
+tempTree.Branch('temp_0600', t0600, 't9_0600/D')
+tempTree.Branch('temp_0700', t0700, 't9_0700/D')
+tempTree.Branch('temp_0800', t0800, 't9_0800/D')
+tempTree.Branch('temp_0900', t0900, 't9_0900/D')
+tempTree.Branch('temp_1000', t1000, 't9_1000/D')
+
+#	Calculation the reaction rate and filling TTree branches in the .root file
+#........................................................................................................
+for i in range(n):
+	numRate = calcReactionRate(resonances,temps,alphaThres,masses)
+	
+	#	Setting reaction rates at various temps to each branch
+	#......................
+	t0010[0] = numRate[0]
+	t0015[0] = numRate[1]
+	t0020[0] = numRate[2]
+	t0030[0] = numRate[3]
+	t0040[0] = numRate[4]
+	t0050[0] = numRate[5]
+	t0060[0] = numRate[6]
+	t0070[0] = numRate[7]
+	t0080[0] = numRate[8]
+	t0090[0] = numRate[9]
+	t0100[0] = numRate[10]
+	t0150[0] = numRate[11]
+	t0200[0] = numRate[12]
+	t0250[0] = numRate[13]
+	t0300[0] = numRate[14]
+	t0350[0] = numRate[15]
+	t0400[0] = numRate[16]
+	t0500[0] = numRate[17]
+	t0600[0] = numRate[18]
+	t0700[0] = numRate[19]
+	t0800[0] = numRate[20]
+	t0900[0] = numRate[21]
+	t1000[0] = numRate[22]
+
+	# Filling tree
+	#.............
+	tempTree.Fill()
+
+#	
+#........................................................................................................
+outputfile.Write()
+outputfile.Close()
 
 
 
